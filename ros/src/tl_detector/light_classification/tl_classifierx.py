88a5fccd23a55e3c624d5a8946123b765bbbabb2
solix <soheil.jahanshahi@gmail.com>
2017-11-08 20:45:48 +0100

    FRCnn-inception inference loaded

--- ros/src/tl_detector/light_classification/tl_classifierx.py
+++ ros/src/tl_detector/light_classification/tl_classifierx.py
@@ -19,7 +19,7 @@ class TLClassifierx(object):
 
 	def __init__(self,*args):
 		self.detection = 0
-		model = './light_classification/data/models/sim_retrained_mobilenet.pb'
+		model = 'light_classification/data/models/frcnn-i-11890.pb'
 		self.detection_graph = tf.Graph()
 		config = tf.ConfigProto()
 		config.gpu_options.allow_growth = True
@@ -33,17 +33,24 @@ class TLClassifierx(object):
 				tf.import_graph_def(od_graph_def, name='')
 			self.sess = tf.Session(graph=self.detection_graph, config=config)
 
-		self.input_img = self.detection_graph.get_tensor_by_name('input:0')
-		self.tl_class = self.detection_graph.get_tensor_by_name('final_result:0')
+		self.input_img = self.detection_graph.get_tensor_by_name('image_tensor:0')
+
+		self.tl_class = self.detection_graph.get_tensor_by_name('detection_classes:0')
+
+		self.tl_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+		self.tl_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
 
 	
 	def get_classification(self,img):
-		
-		img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-		img = cv2.resize(img,(224,224))
-		img = np.expand_dims(img,axis=0)
+		img_np = load_image_into_numpy_array(img)
+		img_np = np.expand_dims(img_np,axis=0)
 		with self.detection_graph.as_default():
-			pred_class = self.sess.run(self.tl_class,feed_dict={self.input_img: img})
+			pred_class = self.sess.run(self.tl_class,feed_dict={self.input_img: img_np})
 		self.detection = np.argmax(pred_class)
 		return self.detection
+
+	#from tensorflow object detection api
+	def load_image_into_numpy_array(image):
+		(im_width, im_height) = image.size
+		return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)	
 
